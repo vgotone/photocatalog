7cc4e05c0f83d1943ac31b427f1d8da367830b03
vgotone <vgotone@gmail.com>
2022-09-30 13:29:20 +0800

    the most base function commit, just process mobile photo in the root directory

--- photocatalog.py
+++ photocatalog.py
@@ -11,29 +11,24 @@ import shutil
 
 
 def process_photo():
-    """处理图片，对图片按时间进行归河分组"""
-    src_dir = 'D:\\Fanchaoqun\\20220816\\333\\'
-    des_dir = 'D:\\Temp\\'
-    print(os.path.dirname(src_dir))
-    print(os.path.dirname(des_dir))
+    """处理图片，对图片按时间进行归类分组"""
+    src_root_dir = r'D:\Fanchaoqun\20220816\333'
+    des_root_dir = r'D:\Temp'
 
-    # def get_file_list():
-    file_lists = os.listdir(src_dir)
-    for file_list in file_lists:
-        if file_list.startswith('IMG') and file_list.endswith('jpg'):
-            # print(file_list)
-            des_dir_date = re.search('\d{8}', file_list).group()
-            des_dir_date_year = des_dir_date[0:3]
-            des_dir_date_month = des_dir_date[4:5]
-            print(des_dir_date)
-            if os.path.exists(os.path.join(des_dir, des_dir_date)):
-                shutil.copy(os.path.join(src_dir, file_list), os.path.join(des_dir, des_dir_date))
-            else:
-                os.mkdir(os.path.join(des_dir, des_dir_date))
-                shutil.copy(os.path.join(src_dir, file_list), os.path.join(des_dir, des_dir_date))
-
-            # des_dir_date = file_list[4:12]
-            # print(des_dir_date)
+    for roots, dirs, filenames in os.walk(src_root_dir):
+        for file_list in filenames:
+            # 处理手机图片中IMG_YYYYMMDD_HHMISS.jpg命名结构的文件
+            if file_list.startswith('IMG') and file_list.endswith('jpg'):
+                des_dir_date = re.search(r'\d{8}', file_list).group()
+                des_dir_date_year = des_dir_date[0:4]
+                des_dir_date_month = des_dir_date[4:6]
+                src_file = os.path.join(roots, file_list)
+                des_dir = os.path.join(des_root_dir, des_dir_date_year, des_dir_date_month, des_dir_date)
+                if os.path.exists(des_dir):
+                    shutil.copy(src_file, des_dir)
+                else:
+                    os.makedirs(des_dir)
+                    shutil.copy(src_file, des_dir)
 
 
 process_photo()
